57370312132c8c5ebc06b5f6ccc05d6a9e80a204
Lethu-ptr <sl24020138@biust.ac.bw>
2026-08-27 16:50:59 +0200

    First commit question1.java

---
+++ Question1.java
@@ -0,0 +1,15 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+        String name = "Lethukuthula Shongwe";   //my actual name
+        int age = 22;                           // my actual age
+        double gpa = 4.4;                       // my actual GPA
+        boolean fullTime = true;
+
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("GPA: " +gpa);
+        System.out.println("Full-time: " +fullTime);
+
+    }
+}
